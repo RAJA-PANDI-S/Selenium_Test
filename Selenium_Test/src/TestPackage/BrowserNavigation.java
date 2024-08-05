14004c414050166e242576bcd262bf5a419d7e32
RAJA-PANDI-S <rajapandirp2712@gmail.com>
2024-08-05 23:00:07 +0530

    Initial Commit

---
+++ Selenium_Test/src/TestPackage/BrowserNavigation.java
@@ -0,0 +1,35 @@
+package TestPackage;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;  
+import org.openqa.selenium.WebDriver;  
+import org.openqa.selenium.chrome.ChromeDriver;  
+
+public class BrowserNavigation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Raja\\chromedriver-win64\\chromedriver.exe");  
+		    WebDriver driver=new ChromeDriver();  
+		      
+		// Launch website  
+		    driver.navigate().to("http://www.google.com/");  
+		    driver.manage().window().maximize();  
+		          
+		    // Click on the search text box and send value  
+		    driver.findElement(By.xpath("//*[@id=\"APjFqb\"]")).sendKeys("Hello World");  
+		          
+		    // Click on the search button  
+		  driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div/div[4]/center/input[1]")).click();  
+		  
+		//  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		  // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);  
+		  
+		    driver.quit();
+		    }  
+		  
+		}  
+
